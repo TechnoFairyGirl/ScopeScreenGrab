86ac724d015e748f733a455f1846c80a2bdaa031
Lily Finley <TechnoFairyGirl@users.noreply.github.com>
2022-03-24 20:22:32 -0500

    Initial code commit.

---
+++ MainWindow.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace ScopeScreenGrab
+{
+	partial class MainWindow
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.hostText = new System.Windows.Forms.Label();
+			this.addressText = new System.Windows.Forms.Label();
+			this.hostBox = new System.Windows.Forms.TextBox();
+			this.addressBox = new System.Windows.Forms.TextBox();
+			this.screenshotBox = new System.Windows.Forms.PictureBox();
+			this.getScreenshotButton = new System.Windows.Forms.Button();
+			this.saveScreenshotButton = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.screenshotBox)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// hostText
+			// 
+			this.hostText.AutoSize = true;
+			this.hostText.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.hostText.Location = new System.Drawing.Point(28, 25);
+			this.hostText.Name = "hostText";
+			this.hostText.Size = new System.Drawing.Size(133, 18);
+			this.hostText.TabIndex = 0;
+			this.hostText.Text = "GPIB Adapter Host";
+			// 
+			// addressText
+			// 
+			this.addressText.AutoSize = true;
+			this.addressText.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.addressText.Location = new System.Drawing.Point(28, 60);
+			this.addressText.Name = "addressText";
+			this.addressText.Size = new System.Drawing.Size(101, 18);
+			this.addressText.TabIndex = 2;
+			this.addressText.Text = "GPIB Address";
+			// 
+			// hostBox
+			// 
+			this.hostBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.hostBox.Location = new System.Drawing.Point(174, 24);
+			this.hostBox.Name = "hostBox";
+			this.hostBox.Size = new System.Drawing.Size(292, 24);
+			this.hostBox.TabIndex = 1;
+			// 
+			// addressBox
+			// 
+			this.addressBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.addressBox.Location = new System.Drawing.Point(174, 59);
+			this.addressBox.Name = "addressBox";
+			this.addressBox.Size = new System.Drawing.Size(61, 24);
+			this.addressBox.TabIndex = 3;
+			// 
+			// screenshotBox
+			// 
+			this.screenshotBox.BackColor = System.Drawing.Color.Black;
+			this.screenshotBox.Location = new System.Drawing.Point(31, 104);
+			this.screenshotBox.Name = "screenshotBox";
+			this.screenshotBox.Size = new System.Drawing.Size(660, 500);
+			this.screenshotBox.TabIndex = 4;
+			this.screenshotBox.TabStop = false;
+			// 
+			// getScreenshotButton
+			// 
+			this.getScreenshotButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.getScreenshotButton.Location = new System.Drawing.Point(31, 626);
+			this.getScreenshotButton.Name = "getScreenshotButton";
+			this.getScreenshotButton.Size = new System.Drawing.Size(146, 39);
+			this.getScreenshotButton.TabIndex = 4;
+			this.getScreenshotButton.Text = "Get Screenshot";
+			this.getScreenshotButton.UseVisualStyleBackColor = true;
+			this.getScreenshotButton.Click += new System.EventHandler(this.getScreenshotButton_Click);
+			// 
+			// saveScreenshotButton
+			// 
+			this.saveScreenshotButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.saveScreenshotButton.Location = new System.Drawing.Point(198, 626);
+			this.saveScreenshotButton.Name = "saveScreenshotButton";
+			this.saveScreenshotButton.Size = new System.Drawing.Size(146, 39);
+			this.saveScreenshotButton.TabIndex = 5;
+			this.saveScreenshotButton.Text = "Save Screenshot";
+			this.saveScreenshotButton.UseVisualStyleBackColor = true;
+			this.saveScreenshotButton.Click += new System.EventHandler(this.saveScreenshotButton_Click);
+			// 
+			// MainWindow
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(730, 690);
+			this.Controls.Add(this.saveScreenshotButton);
+			this.Controls.Add(this.getScreenshotButton);
+			this.Controls.Add(this.screenshotBox);
+			this.Controls.Add(this.addressBox);
+			this.Controls.Add(this.hostBox);
+			this.Controls.Add(this.addressText);
+			this.Controls.Add(this.hostText);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "MainWindow";
+			this.Text = "Oscilloscope Screenshot Tool";
+			((System.ComponentModel.ISupportInitialize)(this.screenshotBox)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label hostText;
+		private System.Windows.Forms.Label addressText;
+		private System.Windows.Forms.TextBox hostBox;
+		private System.Windows.Forms.TextBox addressBox;
+		private System.Windows.Forms.PictureBox screenshotBox;
+		private System.Windows.Forms.Button getScreenshotButton;
+		private System.Windows.Forms.Button saveScreenshotButton;
+	}
+}
+
---
+++ MainWindow.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using SimpleHttp;
+
+namespace ScopeScreenGrab
+{
+	public partial class MainWindow : Form
+	{
+		Config config = null;
+
+		static byte[] GetPclScreenshot(string adapterHost, int gpibAddress)
+		{
+			var client = new TcpClient(adapterHost, 1234);
+			var stream = client.GetStream();
+
+			stream.WriteLine("++savecfg 0");
+			stream.WriteLine("++mode 1");
+			stream.WriteLine($"++addr {gpibAddress}");
+			stream.WriteLine("++auto 0");
+			stream.WriteLine("++eoi 1");
+			stream.WriteLine("++eos 2");
+			stream.WriteLine("++eot_enable 0");
+			stream.WriteLine("++eot_char 0");
+			stream.WriteLine("++read_tmo_ms 3000");
+			stream.WriteLine("++ifc");
+
+			stream.WriteLine(":HARDCOPY:MODE THINKJET");
+			stream.WriteLine(":HARDCOPY:LENGTH 11");
+			stream.WriteLine(":HARDCOPY:PAGE AUTOMATIC");
+			stream.WriteLine(":PRINT?");
+			stream.WriteLine("++read eoi");
+
+			var pclData = new List<byte>();
+
+			while (true)
+			{
+				Task.Delay(3000).Wait();
+				var dataAvailable = client.Available;
+				if (dataAvailable == 0) break;
+				var dataBlock = stream.ReadBytes(dataAvailable);
+				pclData.AddRange(dataBlock);
+			}
+
+			stream.WriteLine("++loc");
+
+			return pclData.ToArray();
+		}
+
+		static int FindBytes<T>(IEnumerable<T> source, IEnumerable<T> pattern)
+		{
+			for (int i = 0; i < source.Count(); i++)
+			{
+				if (source.Skip(i).Take(pattern.Count()).SequenceEqual(pattern))
+					return i;
+			}
+
+			return -1;
+		}
+
+		static byte[] GetImageFromPcl(byte[] pclData)
+		{
+			var rowHeader = new byte[] { 0x1b, 0x2a, 0x62, 0x38, 0x30, 0x57 };
+			var rowLength = 80;
+
+			var imageData = new List<byte>();
+			var inputPtr = 0;
+
+			while (true)
+			{
+				var rowPtr = FindBytes(pclData.Skip(inputPtr), rowHeader);
+				if (rowPtr == -1) break;
+				var row = pclData.Skip(inputPtr + rowPtr + rowHeader.Length).Take(rowLength).ToArray();
+				inputPtr += rowPtr + rowHeader.Length + rowLength;
+				imageData.AddRange(row);
+			}
+
+			return imageData.ToArray();
+		}
+
+		void RenderImage(byte[] imageData)
+		{
+			var width = 640;
+			var height = 480;
+			var border = 10;
+
+			var fullWidth = width + (border * 2);
+			var fullHeight = height + (border * 2);
+
+			var bitmap = new Bitmap(fullWidth, fullHeight);
+
+			for (var y = 0; y < fullHeight; y++)
+				for (var x = 0; x < fullWidth; x++)
+					bitmap.SetPixel(x, y , Color.FromArgb(0, 0, 0));
+
+			var pixelCtr = 0;
+
+			for (var y = 0; y < height; y++)
+			{
+				for (var x = 0; x < width; x++)
+				{
+					var bytePtr = Math.Min(pixelCtr / 8, imageData.Length - 1);
+					var bitPtr = pixelCtr % 8;
+					var bit = Convert.ToString(imageData[bytePtr], 2).PadLeft(8, '0')[bitPtr];
+					var color = bit == '1' ? 255 : 0;
+					pixelCtr++;
+					bitmap.SetPixel(x + border, y + border, Color.FromArgb(color, color, color));
+				}
+			}
+
+			if (screenshotBox.Image != null) screenshotBox.Image.Dispose();
+			screenshotBox.Image = bitmap;
+		}
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			config = JsonConvert.DeserializeObject<Config>(File.ReadAllText("config.json"));
+			hostBox.Text = config.AdapterHost;
+			addressBox.Text = config.GPIBAddress.ToString();
+		}
+
+		private void getScreenshotButton_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				var adapterHost = hostBox.Text;
+				var gpibAddress = int.Parse(addressBox.Text);
+
+				this.Text = "Oscilloscope Screenshot Tool - Working...";
+
+				hostBox.Enabled = false;
+				addressBox.Enabled = false;
+				getScreenshotButton.Enabled = false;
+				saveScreenshotButton.Enabled = false;
+
+				var pclData = GetPclScreenshot(adapterHost, gpibAddress);
+				var imageData = GetImageFromPcl(pclData);
+				RenderImage(imageData);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.ToString(), "Something Went Wrong", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+			finally
+			{
+				hostBox.Enabled = true;
+				addressBox.Enabled = true;
+				getScreenshotButton.Enabled = true;
+				saveScreenshotButton.Enabled = true;
+
+				this.Text = "Oscilloscope Screenshot Tool";
+			}
+		}
+
+		private void saveScreenshotButton_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				var bitmap = (Bitmap)screenshotBox.Image;
+				if (bitmap == null)
+				{
+					MessageBox.Show("You must get an image before saving.", "Seriously?!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+					return;
+				}
+
+				var sfd = new SaveFileDialog();
+				sfd.Filter = "PNG Files (*.png)|*.png";
+				sfd.DefaultExt = "png";
+				sfd.RestoreDirectory = true;
+
+				if (sfd.ShowDialog() == DialogResult.OK)
+					bitmap.Save(sfd.FileName);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.ToString(), "Something Went Wrong", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+		}
+	}
+
+	sealed class Config
+	{
+		public string AdapterHost { get; set; }
+		public int GPIBAddress { get; set; }
+	}
+}
